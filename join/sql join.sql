84ada8c84a18be1b1afca81705e9f1741c3b3872
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-07-07 17:56:56 +0600

    Основные понятие скрипт с примером JOIN в SQL для таблиц customers и orders

--- join/sql join.sql
+++ join/sql join.sql
@@ -1,11 +1,28 @@
+-- jOIN в SQL — это способ объединить данные из двух или более таблиц, 
+-- чтобы получить общую информацию, связанную между ними.
 
+-- пример :
 
+-- таблица Покупатели — хранит имена клиентов.
 
-CREATE DATABASE school;
+-- таблица Заказы — хранит, какие клиенты что заказали.
+
+-- чтобы узнать: "Кто и что заказал?", 
+-- надо соединить эти таблицы по ID клиента — это и есть JOIN
+
+CREATE DATABASE shop;
+
 
 -- показать все База данных
-
 SHOW DATABASES;
+
+USE shop;
+
+DESCRIBE orders;
+
+
+
+
 
 -- показать табличку
 
@@ -13,19 +30,46 @@ SHOW TABLES;
 
 -- показать результат 
 
-SELECT * FROM teachers;
+SELECT * FROM customers;
+SELECT * FROM orders;
 
 
-CREATE TABLE teachers(
+CREATE TABLE customers(
 id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR (25) NOT NULL,
-specialization VARCHAR (100) NOT NULL
+name VARCHAR(25) NOT NULL
 );
- -- данные teachers
-INSERT INTO teachers (name, specialization) VALUES 
-("Элмирбек", "Java ментор"),
-("Эрбол", "Js ментор"),
-("Кайрат", "PHP ментор");
+
+-- данные customers
+INSERT INTO customers (name) VALUES 
+("Алиса"),
+("Боб"),
+("Чолпон");
+
+
+CREATE TABLE orders (
+id INT AUTO_INCREMENT PRIMARY KEY,
+customer_id INT,
+product VARCHAR(50)
+);
+
+-- данные orders
+INSERT INTO orders (customer_id, product) VALUES 
+(1, "Телефон"),
+(2, "Ноутбук"),
+(2, "наушники");
+
+
+-- Соединение таблицы через JOIN
+
+-- SELECT customers.name AS customer_name, orders.product FROM customers JOIN orders ON customers.id = orders.customer_id;
+
+SELECT customers.name AS customer_name, orders.product
+FROM customers
+JOIN orders ON customers.id = orders.customer_id;
+
+
+
+
 
 
 
